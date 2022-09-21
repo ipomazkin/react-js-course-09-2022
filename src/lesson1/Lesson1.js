31ac6c995621084628f234662e2fb295892fd578
zenetix <mnogolikiy.ilay@gmail.com>
2022-09-21 18:50:16 +0300

    initial commit

---
+++ src/lesson1/Lesson1.js
@@ -0,0 +1,15 @@
+/**
+ * @description The Lesson1 component.
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function Lesson1(props) {
+  return (
+    <div className="">Lesson1</div>
+  );
+}
+
+Lesson1.propTypes = {};
+
+Lesson1.defaultProps = {};
